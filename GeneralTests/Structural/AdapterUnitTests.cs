467e77ee6a2af34b310bbc6f013a4a6dfd864bee
Rafael Pimenta <rafael.pimenta@hexagon.com>
2022-09-20 22:12:34 -0300

    Add property adapter

---
+++ GeneralTests/Structural/AdapterUnitTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+using System;
+using Structural;
+using System.Xml;
+using Structural.Adapter;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GeneralTests.Structural
+{
+    public class AdapterUnitTests
+    {
+        [Test]
+        public void ProptertyAdapterTest()
+        {
+            var stats = new CountryStats();
+            stats.Capitals.Add("France", "Paris");
+            var xs = new XmlSerializer(typeof(CountryStats));
+            var stringBuilder = new StringBuilder();
+            var stringWriter = new StringWriter(stringBuilder);
+            xs.Serialize(stringWriter, stats);
+
+            var xml = new XmlDocument();
+            xml.LoadXml(stringBuilder.ToString());
+            Assert.That(xml, Is.Not.EqualTo(null));
+
+            var newStats = (CountryStats)xs.Deserialize(new StringReader(stringWriter.ToString()));
+            Assert.That(newStats.Capitals["France"], Is.EqualTo("Paris"));
+        }
+
+        [Test]
+        public void AdaterTest()
+        {
+
+            List<VectorObject> vectorObjects = new()
+            {
+                new VectorRectangle(1, 1, 10, 10),
+                new VectorRectangle(3, 3, 6, 6)
+            };
+
+
+            foreach (var vo in vectorObjects)
+            {
+                foreach (var line in vo)
+                {
+                    var adapter = new LineToPointAdapter(line);
+                    adapter.ToList().ForEach(p => Console.WriteLine($"X:{p.X} - Y:{p.Y}"));
+                }
+            }
+
+            Assert.That(true, Is.True);
+        }
+    }
+}
---
+++ Structural/PropertyAdapter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace Structural
+{
+    public class CountryStats
+    {
+        public (string, string)[] CapitalsSerializable
+        {
+            get
+            {
+                return Capitals.Keys.Select(country =>
+                      (country, Capitals[country])).ToArray();
+            }
+            set
+            {
+                Capitals = value.ToDictionary(x => x.Item1, x => x.Item2);
+            }
+        }
+
+        [XmlIgnore]
+        public Dictionary<string, string> Capitals { get; private set; } = new();
+    }
+}
