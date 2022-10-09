8c4e73bdfde89c2a5c16c32403a517eda91f33d7
Rafael Pimenta <rafael.pimenta@hexagon.com>
2022-10-09 15:54:12 -0300

    Add Flyweight

---
+++ GeneralTests/Structural/FlyweightUnitTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using Structural.Decorator;
+using Structural.Facade;
+using System.Collections.Generic;
+using System.Text;
+using System;
+using System.Linq;
+using Structural.Flyweight;
+
+namespace GeneralTests.Structural
+{
+    public class FlyweightUnitTests
+    {
+        public static string RandomString()
+        {
+            Random rand = new Random();
+            return new string(
+            Enumerable.Range(0, 10)
+                     .Select(i => (char)('a' + rand.Next(26))).ToArray());
+        }
+
+        [Test]
+        public void TestMagicSquare()
+        {
+            var firstNames = Enumerable.Range(0, 10).Select(_ => RandomString());
+            var lastNames = Enumerable.Range(0, 10).Select(_ => RandomString());
+
+            var users = new List<User2>();
+            foreach (var firstName in firstNames)
+                foreach (var lastName in lastNames)
+                    users.Add(new User2($"{firstName} {lastName}"));
+
+            Assert.IsTrue(true, "");
+        }
+
+        [Test]
+        public void TestTextFormmating()
+        {
+            var bft = new FormattedText("This is a brave new world");
+            bft.GetRange(10, 15).Capitalize = true;
+            Assert.That(bft.ToString().Substring(10, 5), Is.EqualTo("BRAVE"));
+        }
+    }
+}
---
+++ Structural/Flyweight/UserNames.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Structural.Flyweight
+{
+    public class User
+    {
+        public string FullName { get; }
+        public User(string fullName)
+        {
+            FullName = fullName;
+        }
+    }
+
+    public class User2
+    {
+        private static List<string> strings = new();
+        private int[] names; //flyweight
+
+        public User2(string fullName)
+        {
+            names = fullName.Split(' ').Select(GetOrAdd).ToArray();
+
+            int GetOrAdd(string s)
+            {
+                if (string.IsNullOrEmpty(s))
+                {
+                    throw new ArgumentNullException(s);
+                }
+
+                var idx = strings.IndexOf(s);
+                if (idx == -1)
+                {
+                    idx = strings.Count + 1;
+                    strings[idx] = s;
+                }
+                return idx;
+            }
+        }
+
+        public string FullName => string.Join(" ", names.Select(i => strings[i]));
+    }
+}
