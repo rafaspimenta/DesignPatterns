83d6f8ff6e643f83df099a15a48cc1c56bb1a116
Rafael Pimenta <rafael.pimenta@hexagon.com>
2022-10-09 21:30:09 -0300

    - Add Behavioral design pattern group - Add chain of responsibility design pattern

---
+++ Behavioral/ChainOfResponsability/MethodChain.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+namespace Behavioral.ChainOfResponsability
+{
+    public class Creature
+    {
+        private readonly Game game;
+        private int attack, defense;
+        public Creature(Game game, string name, int attack, int defense)
+        {
+            this.game = game;
+            Name = name;
+            this.attack = attack;
+            this.defense = defense;
+        }
+
+        public string Name;
+
+        public int Attack
+        {
+            get 
+            { 
+                var q = new Query(Name, Query.Argument.Attack, attack);
+                game.PerformQuery(this, q);
+                return q.Value;
+            }
+        }
+
+        public int Defense
+        {
+            get
+            {
+                var q = new Query(Name, Query.Argument.Defense, defense);
+                game.PerformQuery(this, q);
+                return q.Value;
+            }
+        }
+    }
+
+    public abstract class CreatureModifier : IDisposable
+    {
+        protected Game game;
+        protected Creature creature;
+        protected CreatureModifier next;
+
+        public CreatureModifier(Game game, Creature creature)
+        {
+            this.game = game;
+            this.creature = creature;
+            game.Queries += Handle;
+        }
+
+        public void Dispose()
+        {
+            game.Queries -= Handle;
+            GC.SuppressFinalize(this);
+        }
+
+        public abstract void Handle(object sender, Query query);
+    }
+
+    public class DoubleAttackModifier : CreatureModifier
+    {
+        public DoubleAttackModifier(Game game, Creature creature)
+          : base(game, creature) { }
+        public override void Handle(object sender, Query query)
+        {
+            if (query.CreatureName == creature.Name && query.WhatToQuery == Query.Argument.Attack)
+            {
+                Console.WriteLine($"Doubling {creature.Name}'s attack");
+                query.Value *= 2;
+
+            }
+        }
+    }
+
+    public class IncreaseDefenseModifier : CreatureModifier
+    {
+        public IncreaseDefenseModifier(Game game, Creature creature)
+          : base(game, creature) { }
+        public override void Handle(object sender, Query query)
+        {
+            if (query.CreatureName == creature.Name && query.WhatToQuery == Query.Argument.Defense)
+            {
+                if (creature.Attack <= 2)
+                {
+                    Console.WriteLine($"Increasing {creature.Name}'s defense");
+                    query.Value++;
+                }
+            }
+        }
+    }
+
+    public class NoBonusesModifier : CreatureModifier
+    {
+        public NoBonusesModifier(Game game, Creature creature)
+          : base(game, creature) { }
+        public override void Handle(object sender, Query query)
+        {
+            Console.WriteLine("No bonuses for you!");
+            // no call to base.Handle() here
+        }
+    }
+
+    public class Game // mediator pattern
+    {
+        public event EventHandler<Query> Queries; // effectively a chain
+        public void PerformQuery(object sender, Query q)
+        {
+            Queries?.Invoke(sender, q);
+        }
+    }
+
+    public class Query
+    {
+        public string CreatureName;
+        public enum Argument
+        {
+            Attack, Defense
+        }
+        public Argument WhatToQuery;
+        public int Value; // bidirectional!
+
+        public Query(string name, Argument argument, int value)
+        {
+            CreatureName = name;
+            WhatToQuery = argument;
+            Value = value;
+        }
+    }
+
+}
---
+++ GeneralTests/Behavioral/ChainOfResponsabilityUnitTests.cs
@@ -0,0 +1,32 @@
+﻿using Behavioral.ChainOfResponsability;
+using NUnit.Framework;
+
+namespace GeneralTests.Behavioral
+{
+    public class ChainOfResponsabilityUnitTests
+    {
+        [Test]
+        public void CreatureModifierTest()
+        {
+            var game = new Game();
+            var goblin = new Creature(game, "Goblin", 2, 2);
+            Assert.That(goblin.Attack, Is.EqualTo(2));
+            Assert.That(goblin.Defense, Is.EqualTo(2));
+
+            using (var da1 = new DoubleAttackModifier(game, goblin)) 
+            {
+                Assert.That(goblin.Attack, Is.EqualTo(4));
+            }
+
+            using (var da2 = new DoubleAttackModifier(game, goblin)) 
+            { 
+                Assert.That(goblin.Attack, Is.EqualTo(4));
+            }
+
+            using (var id1 = new IncreaseDefenseModifier(game, goblin)) 
+            {
+                Assert.That(goblin.Defense, Is.EqualTo(3));
+            }
+        }
+    }
+}
